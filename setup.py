8b1a0c63d2c40d152ff6c4217a5800b7bc8c3885
eivindtn <eivindtn@stud.ntnu.no>
2020-10-14 12:57:16 +0200

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="Project-Eivind-Nerol", 
+    version="0.0.1",
+    python_requires='>=3.5, <3.8',
+    install_requires =[
+        'zivid'
+        'opencv-python'
+    ]
+)
